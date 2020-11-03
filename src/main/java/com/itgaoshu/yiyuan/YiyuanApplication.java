81e22f349bd48b58e8d504ea5c87036c0685c097
Administrator <1548338495@qq.com>
2020-11-03 09:02:46 +0800

    启动类上添加了@MapperScan

--- src/main/java/com/itgaoshu/yiyuan/YiyuanApplication.java
+++ src/main/java/com/itgaoshu/yiyuan/YiyuanApplication.java
@@ -1,9 +1,11 @@
 package com.itgaoshu.yiyuan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class YiyuanApplication {
 
     public static void main(String[] args) {
