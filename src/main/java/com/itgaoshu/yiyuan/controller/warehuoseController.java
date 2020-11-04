04c66cc93ad5560e696255152f56e25be393fa17
DELL <https://github.com/Heilaogou/Medical.git>
2020-11-04 09:46:35 +0800

    终于在前台页面显示出来了！注意实体类里面的属性值跟html页面传的值名字是否相同

--- src/main/java/com/itgaoshu/yiyuan/controller/warehuoseController.java
+++ src/main/java/com/itgaoshu/yiyuan/controller/warehuoseController.java
@@ -4,8 +4,10 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.itgaoshu.yiyuan.bean.Warehuose;
 import com.itgaoshu.yiyuan.service.warehuoseService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,36 +16,47 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("warehouse")
 public class warehuoseController {
     @Autowired
     private warehuoseService warehuoseservice;
+    //新增仓库
     @RequestMapping("addWarehuose")
     @ResponseBody
     public String addWarehuose(Warehuose wh){
-       int result=warehuoseservice.addWarehuose(wh);
-       if(result==1){
-           return "新增成功";
-       }else{
-           return "新增失败，请重新新增";
+       int count=warehuoseservice.count(wh);
+       if(count==0){
+           int result=warehuoseservice.addWarehuose(wh);
+           if(result==1){
+               return "新增成功";
+           }else{
+               return "新增失败，请重新新增";
+           }
+       }else {
+            return wh.getSupplierName()+"已存在，请重新输入!";
        }
     }
+    //删除仓库
     @RequestMapping("deleteWarehuose")
     @ResponseBody
     public String deleteWarehuose(Integer warehuoseId){
-        int result=warehuoseservice.deletewarehuose(warehuoseId);
+        int result=warehuoseservice.deleteWarehuose(warehuoseId);
         if(result==1){
             return "删除成功";
         }else{
             return "删除失败";
         }
     }
+    //模糊查询仓库
     @RequestMapping("findAllWarehuose")
     @ResponseBody
-    public Object findAllWarehuose(Warehuose wh,Integer page,Integer size){
-        PageHelper.startPage(page,size);
+    public Object findAllWarehuose(Warehuose wh, Integer page, Integer limit){
+        PageHelper.startPage(page,limit);
         List<Warehuose> whlist=warehuoseservice.findAllWarehuose(wh);
         PageInfo pageInfo=new PageInfo(whlist);
         Map<String,Object> map=new HashMap();
+        map.put("code", 0);
+        map.put("msg", "");
         //总记录数
         map.put("count",pageInfo.getTotal());
         //结果集
--- src/main/java/com/itgaoshu/yiyuan/mapper/WarehuoseMapper.java
+++ src/main/java/com/itgaoshu/yiyuan/mapper/WarehuoseMapper.java
@@ -2,31 +2,30 @@ package com.itgaoshu.yiyuan.mapper;
 
 import com.itgaoshu.yiyuan.bean.Warehuose;
 import com.itgaoshu.yiyuan.bean.WarehuoseExample;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface WarehuoseMapper {
-    int countByExample(WarehuoseExample example);
 
-    int deleteByExample(WarehuoseExample example);
-
-    int deleteByPrimaryKey(Integer warehouseid);
-
-    int insert(Warehuose record);
-
-    int insertSelective(Warehuose record);
-
-    List<Warehuose> selectByExample(WarehuoseExample example);
+    //根据主键删除仓库
+    int deleteWarehuose(Integer warehouseid);
+    //新增仓库
+    int addWarehuose(Warehuose record);
     //自己新增的模糊查询方法
     List<Warehuose> findAllWarehuose(Warehuose wh);
+    int count(Warehuose wh);
+
+
+    /*int insertSelective(Warehuose record);
+    int countByExample(WarehuoseExample example);
+    int deleteByExample(WarehuoseExample example);
+    List<Warehuose> selectByExample(WarehuoseExample example);
     Warehuose selectByPrimaryKey(Integer warehouseid);
-
     int updateByExampleSelective(@Param("record") Warehuose record, @Param("example") WarehuoseExample example);
-
     int updateByExample(@Param("record") Warehuose record, @Param("example") WarehuoseExample example);
-
     int updateByPrimaryKeySelective(Warehuose record);
-
-    int updateByPrimaryKey(Warehuose record);
+    int updateByPrimaryKey(Warehuose record);*/
 }
