1f8e3c6c3961169941ef941814f54e985d93e739
ghw961020 <981568878@qq.com>
2020-11-04 20:30:38 +0800

    入院登记的相关操作

--- src/main/java/com/itgaoshu/yiyuan/bean/Register.java
+++ src/main/java/com/itgaoshu/yiyuan/bean/Register.java
@@ -1,8 +1,28 @@
 package com.itgaoshu.yiyuan.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Register {
+    /**
+     * 患者信息
+     *  registerid	int	Not null auto_increment	Primary key,	-- 序号
+     * userName	Varchar(200)	Not null		,	-- 患者名字
+     * age	Int	Not null		,	-- 年龄
+     * sex	Varchar(20)	Not null		,	-- 性别
+     * department	int	Not null		,	-- 科室 FOREIGN
+     * doctor	int	Not null		,	-- 医生FOREIGN
+     * diagnose	Varchar(500)			,	-- 入院诊断
+     * address	Varchar(500)			,	-- 地址
+     * Phone	Varchar(30)			,	-- 电话
+     * Idcard	Varchar(60)			,	-- 身份证号
+     * registerDate	Date			,	-- 入院日期
+     * bedNum	int ,	-- 床号FOREIGN
+     * Operator	Varchar(200)			,	-- 操作员
+     * money	double			,	-- 预交金
+     * state	Int				-- 状态
+     */
     private Integer registerid;
 
     private String username;
@@ -11,9 +31,13 @@ public class Register {
 
     private String sex;
 
-    private Integer department;
+    private Integer departmentid;
+
+    private String department;
 
     private Integer doctor;
+
+    private String doctorname;
 
     private String diagnose;
 
@@ -22,17 +46,19 @@ public class Register {
     private String phone;
 
     private String idcard;
-
+    @JsonFormat(pattern = "yyyy年MM月dd日",timezone = "GMT+8" )
     private Date registerdate;
 
     private Integer bednum;
+
+    private String bedname;
 
     private String operator;
 
     private Double money;
 
     private Integer state;
-
+    @JsonFormat(pattern = "yyyy年MM月dd日",timezone = "GMT+8" )
     private Date enddate;
 
     private Double price;
@@ -40,6 +66,46 @@ public class Register {
     private String discount;
 
     private Date zhuan;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getDepartmentid() {
+        return departmentid;
+    }
+
+    public void setDepartmentid(Integer departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public String getDoctorname() {
+        return doctorname;
+    }
+
+    public void setDoctorname(String doctorname) {
+        this.doctorname = doctorname;
+    }
+
+    public Date getRegisterdate() {
+        return registerdate;
+    }
+
+    public void setRegisterdate(Date registerdate) {
+        this.registerdate = registerdate;
+    }
+
+    public String getBedname() {
+        return bedname;
+    }
+
+    public void setBedname(String bedname) {
+        this.bedname = bedname;
+    }
 
     public Integer getRegisterid() {
         return registerid;
@@ -49,13 +115,7 @@ public class Register {
         this.registerid = registerid;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username == null ? null : username.trim();
-    }
 
     public Integer getAge() {
         return age;
@@ -73,11 +133,13 @@ public class Register {
         this.sex = sex == null ? null : sex.trim();
     }
 
-    public Integer getDepartment() {
+
+
+    public String getDepartment() {
         return department;
     }
 
-    public void setDepartment(Integer department) {
+    public void setDepartment(String department) {
         this.department = department;
     }
 
@@ -121,13 +183,7 @@ public class Register {
         this.idcard = idcard == null ? null : idcard.trim();
     }
 
-    public Date getRegisterdate() {
-        return registerdate;
-    }
 
-    public void setRegisterdate(Date registerdate) {
-        this.registerdate = registerdate;
-    }
 
     public Integer getBednum() {
         return bednum;
@@ -192,4 +248,5 @@ public class Register {
     public void setZhuan(Date zhuan) {
         this.zhuan = zhuan;
     }
+
 }
--- src/main/java/com/itgaoshu/yiyuan/controller/RegisterController.java
+++ src/main/java/com/itgaoshu/yiyuan/controller/RegisterController.java
@@ -2,6 +2,9 @@ package com.itgaoshu.yiyuan.controller;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.itgaoshu.yiyuan.bean.Bed;
+import com.itgaoshu.yiyuan.bean.Departments;
+import com.itgaoshu.yiyuan.bean.Doctor;
 import com.itgaoshu.yiyuan.bean.Register;
 import com.itgaoshu.yiyuan.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,16 +23,23 @@ import java.util.Map;
 public class RegisterController {
 
     @Autowired
+    //对患者的操作
     private RegisterService registerService;
 
-    //分页查询全部已登记信息
-    @RequestMapping("selectAllRegister")
+    //跳转入院登记页面
+    @RequestMapping("/admin")
+    public String admin(){
+        return "liao/admin";
+    }
+
+    //分页查询患者信息
+    @RequestMapping("selRegister")
     @ResponseBody
-    public Object selectAllRegister(Model model,Integer page,Integer limit,Register register){
+    public Object selRegister(Model model,Integer page,Integer limit,Register register){
         //分页查询
         PageHelper.startPage(page,limit);
         //查询未出院的患者信息
-        List<Register> selreg = registerService.selectAllRegister(register);
+        List<Map<String,Object>> selreg = registerService.selectAllRegister(register);
         //查出的信息存储到分页插件中
         PageInfo pageInfo = new PageInfo(selreg);
         Map<String,Object> tableDate = new HashMap<String, Object>();
@@ -40,7 +51,80 @@ public class RegisterController {
         return tableDate;
     }
 
+    /**
+     *页面请求查询科室信息
+     */
+    @RequestMapping("/selDepartment")
+    @ResponseBody
+    public Object selDepartment(){
+        Departments department=new Departments();
+        //查询科室
+        List<Departments> departments= registerService.selectAllDepartment(department);
+        //把查到的科室返回前台下拉框中
+        return departments;
+    }
 
+    /**
+     * 查询医生信息
+     */
+    @RequestMapping("/selDoctor")
+    @ResponseBody
+    public Object selDoctor(Integer departmentId){
+        Doctor doctor = new Doctor();
+        doctor.setDepartmentid(departmentId);
+        System.out.println(doctor.getDepartmentid());
+        List<Doctor> doctors = registerService.selectAllDoctor(doctor);
+        return doctors;
+    }
 
+    /**
+     * 查询空床位
+     */
+    @RequestMapping("/selBed")
+    @ResponseBody
+    public Object selBed(Integer departmentid){
+        Bed bed = new Bed();
+        bed.setDepartmentid(departmentid);
+        //床位状态为0
+        bed.setState(0);
+        List<Bed> beds = registerService.selBed(bed);
+        return beds;
+    }
+
+    /**
+     * 添加患者
+     */
+    /*@RequestMapping("/addRegister")
+    @ResponseBody
+    public Object addRegister(Register register, HttpServletRequest request){
+        Register register1=new Register();
+        register1.setIdcard(register.getIdcard());
+        //把患者的身份证号查询一下看看是否已经办理入住
+        List<Register> registers = ladminService.selRegister(register1);
+        if(registers.size()==0){
+            //如果是门诊转过来的用户删除门诊患者信息
+            if(register.getReportid()!=0){
+                ladminService.updZ(register);
+            }
+            //获取管理人员姓名
+            String yonghu = (String)request.getSession().getAttribute("yonghu");
+            register.setOperator(yonghu);
+            //添加患者信息
+            int i = ladminService.addRegister(register);
+            if(i==1){
+                //修改床位状态
+                int j = ladminService.updBed(register);
+                if(j==1){
+                    return "添加成功";
+                }else{
+                    return "添加失败";
+                }
+            }else{
+                return "添加失败";
+            }
+        }else{
+            return "该患者已存在，请不要重复添加！";
+        }
+    }*/
 
 }
