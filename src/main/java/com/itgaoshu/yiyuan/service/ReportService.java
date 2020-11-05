feed7ca81ff126e66356c9852a3db9f61229ebb4
Administrator <1548338495@qq.com>
2020-11-05 17:01:27 +0800

    bug警告！！！请勿在门诊管理菜单乱点，仅实现了门诊管理的大体流程，仍有许多bug，先存档休息休息

--- src/main/java/com/itgaoshu/yiyuan/service/ReportService.java
+++ src/main/java/com/itgaoshu/yiyuan/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.itgaoshu.yiyuan.service;
 
 import com.itgaoshu.yiyuan.bean.*;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,9 +29,11 @@ public interface ReportService {
     int addre(Report r);
     //删除挂号
     int delre(int id);
+    //转住院
+    Integer zhuanyuan(Integer id,String zhuan);
     //======================================处方划价=========================================
     //根据挂号id查询病人的处方信息
-    List<Cashier> selcha(Integer perid);
+    List<Cashier> selall(Integer perid);
     //查询药房中的所有药品
     List<Pharmacy> seldrug(String drugname);
     //根据挂号id查询病人病因
@@ -58,4 +61,31 @@ public interface ReportService {
     List<Cashier> selxiang(Integer perid);
     //根据挂号id查询当前病人要缴纳的总费用
     Double selshoux(Integer perid);
+    //根据挂号id缴费
+    Integer shoufei(Integer perid);
+
+    //=========================================项目检查=====================================
+    //根据挂号id查询需要检查的项目
+    List<Cashier> selcha(Integer perid);
+    //病人做完需要检查的项目后填写检查到的病因
+    Integer addbingc(Integer reid,String bing,Integer cashier);
+
+    //=========================================药品缴费=====================================
+    //根据挂号id查询当前病人处方中的所有药品信息
+    List<Cashier> selpepi(Integer perid);
+    //根据挂号id查询是否有未缴费的项目
+    Integer seljiao(Integer reid);
+    //根据挂号id查询已缴费但未检查的项目
+    Integer selwei(Integer reid);
+    //根据挂号id查询处方中药品总价
+    Double selch(Integer reportId);
+    //根据挂号id缴纳药品费用，并且缴纳挂号费用，并且修改病人状态report.state=2表示该病人看诊结束
+    Integer shoufeic(Integer reid,Double price);
+
+    //=========================================门诊患者库(病人就诊结束(直到完成取药)后自动加入门诊患者库)=====================================
+    //模糊查询所有患者
+    List<Report> selhuan(String name);
+    //根据挂号id查询病人挂号的总费用
+    Double zong(Integer reid);
+
 }
